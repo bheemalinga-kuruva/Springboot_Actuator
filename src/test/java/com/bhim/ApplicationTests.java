196d81bfe8536f1b35ea73683e540a256294378a
bheemalinga-kuruva <javabybhim1996@gmail.com>
2024-10-11 12:46:31 +0530

    first commit

---
+++ src/test/java/com/bhim/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
